c419de20764e6debc64918ad9355259d64b6cdb0
Wintory <wintory@users.noreply.github.com>
2018-06-01 18:39:52 +0700

    complete add,remove pokedex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
+import axios from 'axios'
+import Card from './Card'
 
 const COLORS = {
   Psychic: "#f8a5c2",
@@ -15,10 +17,118 @@ const COLORS = {
   Fire: "#eb4d4b"
 }
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      allPokemon: [],
+      pokemonResult: [],
+      pokedex: [],
+      pokeid: [],
+      searchPokemon: '',
+      pokemonName: '',
+      searchbox: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getAllPokemon()
+  }
+
+  setData = (data) => {
+    this.setState({
+      allPokemon: data
+    })
+  }
+
+  addCard = (id) => {
+
+    this.state.allPokemon.filter((pokemon) => {
+      if (pokemon.id === id) {
+        return this.setState({ pokedex: [...this.state.pokedex, pokemon], pokeid: [...this.state.pokeid, id] })
+      }
+    })
+
+    console.log(this.state.pokedex);
+
+  }
+
+  deleteCard = (id) => {
+    this.setState({
+      pokedex: this.state.pokedex.filter((data) => { if (data.id !== id) { return data } }),
+      pokeid: this.state.pokeid.filter((data) => { if (data !== id) { return data } }),
+    })
+  }
+
+  searchbox = (message) => {
+    this.setState({
+      searchPokemon: message
+    })
+  }
+
+  getAllPokemon = () => {
+    axios.get('http://localhost:3030/api/cards')
+      .then(function (response) {
+        if (response.status === 200) {
+          return response.data.cards
+        }
+      }).then((data) => this.setState({ allPokemon: data }))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  getFromSearch = (val) => {
+    axios.get('http://localhost:3030/api/cards')
+      .then(function (response) {
+        if (response.data.cards) {
+          return response.data.cards.filter((data) => { })
+        }
+      }).then((data) => this.setState({ allPokemon: data }))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
+
+
     return (
       <div className="App">
+        <div className='container'>
+          <div className='row'>
+            <nav className='header'>
+              <p className='textheader'>My Pokedex</p>
+            </nav>
+            <div className='col-12'>
+            </div>
+            {this.state.pokedex.map((data, i) => {
+              return (
+                <div className='col-6'>
+                  <Card img={data.imageUrl} status='true' weakness={data.weaknesses} attacks={data.attacks} deleteCard={this.deleteCard} id={data.id} hp={data.hp} Pokename={data.name} key={i} />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        <footer className='footer'>
+          <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            +
+</button>
+        </footer>
+        <input type='text' placeholder='pokemon name' onChange={(e) => this.searchbox(e.target.value)} />
+        {this.state.allPokemon.map((data, i) => {
+          if (this.state.pokeid.indexOf(data.id) === -1) {
+            return (
+              <div className='col-6'>
+                <Card img={data.imageUrl} status='false' weakness={data.weaknesses} attacks={data.attacks} addCard={this.addCard} id={data.id} hp={data.hp} Pokename={data.name} key={i + data.id} />
+              </div>
+            )
+          }
+        })}
+
       </div>
     )
   }
---
+++ src/Card.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+
+    render() {
+
+
+        return (
+            <div className='card'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-4'>
+                            <img className='cardimg' src={this.props.img} style={{ width: 100 }} />
+                        </div>
+                        <div className='col-6'>
+                            <span>name : {this.props.Pokename}</span>
+                            <br />
+                            <span>hp : {this.props.hp >= 100 ? 100 : 0}</span>
+                            <br />
+                            <span>str : </span>
+                            <br />
+                            <span>weak : </span>
+                            <br />
+                            <span>damage : </span>
+                            <br />
+                            {/* <span>weak : {this.props.weakness}</span> */}
+                        </div>
+                        {this.props.status === 'false' ? <button className='col-2' onClick={() => this.props.addCard(this.props.id)}>
+                            add
+                        </button> : <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => this.props.deleteCard(this.props.id)}>
+                                <span aria-hidden="true">&times;</span>
+                            </button>}
+                    </div>
+                </div>
+            </div >
+        )
+    }
+}
